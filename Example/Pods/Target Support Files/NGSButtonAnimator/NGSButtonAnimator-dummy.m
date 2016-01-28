f592b92f76846dca3b3ddbb7c6b8e29ac9a174f4
Paulius Vindzigelskis <paulius.vindzigelskis@barclays.com>
2016-01-28 10:48:01 +0200

    cleanup

--- Example/Pods/Target Support Files/NGSButtonAnimator/NGSButtonAnimator-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NGSButtonAnimator : NSObject
-@end
-@implementation PodsDummy_NGSButtonAnimator
-@end
--- Example/Pods/Target Support Files/Pods-NGSButtonAnimator_Tests/Pods-NGSButtonAnimator_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_NGSButtonAnimator_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_NGSButtonAnimator_TestsVersionString[];
-
